819cf8cd8688b96de467f2d6084b0e6551aa6d02
BarKar96 <karlowski.bartek@gmail.com>
2018-03-23 18:47:18 +0100

    commit

---
+++ Talisman/Assets/Field.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Field {
+
+    public GameObject emptyGameObject;
+    public Field() 
+    {
+        
+    }
+}
---
+++ Talisman/Assets/TalismanBoardScript.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class TalismanBoardScript : MonoBehaviour {
+
+    Field[] outerRing;
+    Field[] middleRing;
+    Field[] innerRing;
+
+    Piece[] pieceArray;
+    Player[] playerArray;
+
+    public GameObject piecePrefab;
+    public int playersCounter;
+
+    private void initializePlayer()
+    {
+        playerArray = new Player[playersCounter];
+        for (int i=0; i<playersCounter; i++)
+        {
+            playerArray[i] = new Player();
+            
+        }
+    }
+
+    private int rollDice()
+    {
+        System.Random rnd = new System.Random();
+        return rnd.Next(1,6);
+    }
+
+    private void fillFields()
+    {
+        outerRing = new Field[24]; 
+        for (int i=0; i<24; i++)
+        {
+            
+            outerRing[i] = new Field();
+            outerRing[i].emptyGameObject =  transform.GetChild(i+1).gameObject;
+           
+        }
+    }
+
+    private void movePiece(int indexOfPieceToMove, int indexOfFieldToMoveOn)
+    {
+       pieceArray[indexOfPieceToMove].transform.position = outerRing[indexOfFieldToMoveOn].emptyGameObject.transform.position;
+    }
+
+
+    private void GenerateBoard()
+    {
+        playersCounter = 1;
+        pieceArray = new Piece[playersCounter];
+        for (int i=0; i<playersCounter; i++)
+        {
+            pieceArray[i] = new Piece();
+            GeneratePiece(i);
+            initializePlayer();
+        }
+        fillFields(); 
+    }
+
+    private Piece GeneratePiece(int i)
+    {
+        GameObject go = Instantiate(piecePrefab) as GameObject;
+        go.transform.SetParent(transform);
+        Piece p = go.GetComponent<Piece>();
+        pieceArray[i] = p;
+        MovePieceToStartLocation(p, i);
+        return p;
+    }
+
+    //do nadpisania wg regul gry
+    private void MovePieceToStartLocation(Piece p, int i)
+    {
+       pieceArray[i].transform.position = transform.GetChild(i+1).gameObject.transform.position;
+       p.indexOfField = i;
+    }
+    public void Button_Click()
+    {
+        int x = rollDice();
+        Debug.Log(x);
+        int y = pieceArray[0].indexOfField;
+        int whereToMove = (x + y) % 24;
+        movePiece(0,whereToMove);
+        pieceArray[0].indexOfField = whereToMove;
+
+    }
+    void Start ()
+    {
+        GenerateBoard();
+        
+	}
+
+	void Update ()
+    {
+	
+	}
+}
