dde492809a95e68d89555c44226182023d63e310
Unknown <towerdweller@protonmail.ch>
2018-04-04 10:24:12 +0200

    Began work on card logic

---
+++ Talisman/Assets/UI_CardLogic.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_CardLogic : MonoBehaviour {
+
+    private GameObject cardItself;
+    //  To be filled with number of possible card textures
+    private int totalCards = 140;
+    //  Array for quick card switching
+    private Material[] cardTextures = new Material[140];
+
+    // Use this for initialization
+    void Start () {
+        cardItself = GameObject.Find("Card");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Face.GetComponent<Renderer>().material.mainTexture = textures[0];
+        Texture newTexture = new Texture();
+        //newTexture.
+        //cardItself.GetComponent<Renderer>().material.mainTexture = 
+    }
+}
