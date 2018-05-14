418a768b91b280b767e5016163375fdbfff94042
Sławomir Asimowicz <Sawomir Asimowicz>
2018-05-14 15:54:42 +0200

    maly up menu dla Darka

--- Talisman/Assets/MainMenu.cs
+++ Talisman/Assets/MainMenu.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour {
 
-	public void NewGame()
+    public void StartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
